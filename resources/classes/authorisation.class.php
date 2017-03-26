72369aac3c570722738064fdc2e9ed84cfe06d4f
Charl Theron <Charl.Theron@standardbank.co.za>
2017-03-26 06:41:13 +0200

    adding base files system.
    also added connect.class that allows you to connect to the database.
    
    and added the password hashing to user.class.php

--- resources/classes/authorisation.class.php
+++ resources/classes/authorisation.class.php
@@ -9,38 +9,5 @@
 require_once ("user.class.php");
 class authorisation extends user_management
 {
-	public function login_user()
-	{
-		$login = $_POST['login'];
-		$_SESSION['user'] = $login;
-		$pass = $_SESSION['password'];
-		$hashPassword = password_hash($pass);
-		$_SESSION['password'] = $hashPassword;
-		$check = $this->query("SELECT USERNAME FROM `users`");
-		foreach ($check as $row) {
-			if ($row['USERNAME'] == $_POST['login']) {
-				$check = $this->query('SELECT PASSWORD FROM users');
-				foreach ($check as $row) {
-					if ($row['PASSWORD'] == $pass) {
-						$check = $this->query('SELECT ACTIVE FROM users');
-						foreach ($check as $row) {
-							if ($row['ACTIVE'] == true) {
-								$_SESSION['login'] = $_POST['login'];
-								$_SESSION['password'] = $_POST['password'];
-								//redirect to main page;
-							} else {
-								//error
-							}
-						}
-					} else {
-						//error
-					}
-				}
-			} else {
-				//error
-			}
-		}
-	}
-
 
 }
